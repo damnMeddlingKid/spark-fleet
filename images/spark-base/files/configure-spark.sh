a125ad47d4c90f14e98d3857e314f8d7aa5cbcd0
damnMeddlingKid <franklynd@gmail.com>
2015-02-26 15:56:20 -0500

    added worker units and images

--- images/spark-base/files/configure-spark.sh
+++ images/spark-base/files/configure-spark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 function configure_spark() {
-	cp /root/spark-config/spark-env.sh /opt/spark-$SPARK_VERSION/conf/
-	cp /root/spark-config/log4j.properties /opt/spark-$SPARK_VERSION/conf/
+	cp /root/spark-config/spark-env.sh ${SPARK_HOME}/conf/
+	cp /root/spark-config/log4j.properties ${SPARK_HOME}/conf/
 }
---
+++ images/spark-worker/files/start-worker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#env
+
+source /root/spark-config/configure-spark.sh
+
+WORKER=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
+
+export WORKER=$IP
+
+configure_spark
+
+${SPARK_HOME}/start-worker.sh
+
+echo "SPARK WORKER STARTED ON spark://$WORKER:8888"
+
+while true;
+do 
+	tail -f ${SPARK_HOME}/logs/*.out
+	sleep 1
+done
+
