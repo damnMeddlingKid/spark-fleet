8f00a4d024c729d904bc8c16861af372ba066daa
frnaklynd <frnaklynd@gmail.com>
2015-03-11 17:28:11 -0400

    split hdfs in worker and master

--- images/hdfs/files/setup-hadoop.sh
+++ images/hdfs/files/setup-hadoop.sh
@@ -6,6 +6,9 @@ mkdir /root/.ssh
 chmod go-rx /root/.ssh
 mkdir /var/run/sshd
 
+cp /root/hadoop-config/core-site.xml /hdfs/hadoop/etc/hadoop/core-site.xml
+cp /root/hadoop-config/hdfs-site.xml /hdfs/hadoop/etc/hadoop/hdfs-site.xml
+
 cp /root/hadoop-config/id_rsa /root/.ssh
 chmod go-rwx /root/.ssh/id_rsa
 cp /root/hadoop-config/authorized_keys /root/.ssh/authorized_keys
