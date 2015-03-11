fe3cd8fca054a0421b10ce6a099c8e2b0fda0f1d
frnaklynd <frnaklynd@gmail.com>
2015-03-11 16:53:07 -0400

    working on hdfs

--- images/hdfs/files/setup-hadoop.sh
+++ images/hdfs/files/setup-hadoop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-function setup_hadoop() {
-	rm -rf /root/.ssh
-	mkdir /root/.ssh
-	chmod go-rx /root/.ssh
-	mkdir /var/run/sshd
+#function setup_hadoop() {
+rm -rf /root/.ssh
+mkdir /root/.ssh
+chmod go-rx /root/.ssh
+mkdir /var/run/sshd
 
-	cp /root/hadoop_files/id_rsa /root/.ssh
-	chmod go-rwx /root/.ssh/id_rsa
-	cp /root/hadoop_files/authorized_keys /root/.ssh/authorized_keys
-	chmod go-wx /root/.ssh/authorized_keys
+cp /root/hadoop-config/id_rsa /root/.ssh
+chmod go-rwx /root/.ssh/id_rsa
+cp /root/hadoop-config/authorized_keys /root/.ssh/authorized_keys
+chmod go-wx /root/.ssh/authorized_keys
 
-	sed -i s/__MASTER__/${MASTER}/ /opt/hadoop/etc/hadoop/core-site.xml
-	
-	hadoop namenode -format
-}
+sed -i s/__MASTER__/${MASTER}/ /hdfs/hadoop/etc/hadoop/core-site.xml
+
+hadoop namenode -format
+#}
 
 
 
